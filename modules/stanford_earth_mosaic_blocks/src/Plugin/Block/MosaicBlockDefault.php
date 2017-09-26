554d8cc451a1b40e5e0ae1a57716bc8658df2c98
Shea McKinney <sherakama@gmail.com>
2017-09-26 16:03:22 -0700

    EARTH-297: With real content.

--- modules/stanford_earth_mosaic_blocks/src/Plugin/Block/MosaicBlockDefault.php
+++ modules/stanford_earth_mosaic_blocks/src/Plugin/Block/MosaicBlockDefault.php
@@ -19,7 +19,161 @@ class MosaicBlockDefault extends BlockBase {
    */
   public function build() {
     return array(
-      '#pattern' => 'section_photo_mosaic_quotes',
+      '#type' => 'pattern',
+      '#id' => 'section_photo_mosaic_quotes',
+      '#fields' => [
+        'dash_under' => FALSE,
+        'is_centered' => FALSE,
+        'is_featured' => FALSE,
+        'subhead' => '<p>' . $this->t('We are scientists! Undergraduates, graduate students, professors, educational staff, and alumni working professionals. We build community in our field trips, classes, and cocurriculars. We care about the Earth and making its resources available to people across the globe now and in the future.') . '</p>',
+        'superhead' => $this->t('Our Community.'),
+        'tiles' => [
+          [
+            'cite_name' => $this->t('Elsa M. Ordway'),
+            'cite_title' => $this->t('Ph.D. Candidate'),
+            'classes' => 'photo-mosaic--thumbs-down-quote',
+            'description' => $this->t('Together, we pursue science and build lifelong bonds.'),
+            'image' => [
+              '#alt' => $this->t('A collection of happy students'),
+              '#theme' => 'image',
+              '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/studentmain.png',
+            ],
+            'is_right' => TRUE,
+            'quote' => $this->t('We face enormous hrdles as a society in dealing with a changing climate, over extraction of resources, and biodiversity loss. Still, I remain optimistic in our ability to identify and design solutions.'),
+            'items' => [
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/student1.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/student2.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/student3.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/student4.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+            ],
+            'link' => 'https://stanford.edu/',
+            'title' => 'Students',
+          ],
+          [
+            'cite_name' => $this->t('Biondo Biondi.'),
+            'cite_title' => $this->t('Geophysics Professor.'),
+            'classes' => 'photo-mosaic--thumbs-up',
+            'description' => $this->t('We discover and teach.'),
+            'image' => [
+              '#alt' => $this->t('this is a sample image'),
+              '#theme' => 'image',
+              '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/facmain.png',
+            ],
+            'is_right' => FALSE,
+            'quote' => $this->t('I\'m fascinated by the way our world works. I use technology to make visible what is not visible to the naked eye.'),
+            'items' => [
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/fac1.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/fac2.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/fac3.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/fac4.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+            ],
+            'link' => 'https://stanford.edu/',
+            'title' => $this->t('Faculty'),
+          ],
+          [
+            'classes' => 'hoto-mosaic--thumbs-down-alt',
+            'description' => $this->t('We connect and keep learning.'),
+            'image' => [
+              '#alt' => $this->t('this is a sample image'),
+              '#theme' => 'image',
+              '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/alummain.png',
+            ],
+            'is_right' => FALSE,
+            'items' => [
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/alum1.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/alum2.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/alum3.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+              [
+                'image' => [
+                  '#alt' => $this->t('this is a sample image'),
+                  '#theme' => 'image',
+                  '#uri' => drupal_get_path('module', 'stanford_earth_mosaic_blocks') . '/img/alum4.png',
+                ],
+                'link' => 'https://www.stanford.edu',
+              ],
+            ],
+            'link' => 'https://stanford.edu/',
+            'title' => $this->t('Alumni'),
+          ],
+        ],
+      ],
     );
   }
 
