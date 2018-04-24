0e37b1c0f4631aa6256637bf86548c2f8b7a84bc
Shea McKinney <sherakama@gmail.com>
2018-04-24 10:27:49 -0700

    EARTH-697: Starting stanford_earth_events.

---
+++ modules/stanford_earth_events/js/script.js
@@ -0,0 +1,13 @@
+(function ($) {
+  'use strict';
+
+  /*
+   *  My script code comment
+   */
+  Drupal.behaviors.stanford_earth_events = {
+    attach: function (context) {
+      // My code here...
+    }
+  };
+
+})(jQuery);
