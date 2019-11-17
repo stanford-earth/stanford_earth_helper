2244b72825e6f015426a5b1360bd7f6e57c40b82
Kenneth R Sharp <ksharp@stanford.edu>
2019-11-16 21:51:14 -0800

    EARTH-1178a Updated to include single sunet import.

--- modules/stanford_earth_capx/src/Form/EarthCapxUpdatePerson.php
+++ modules/stanford_earth_capx/src/Form/EarthCapxUpdatePerson.php
@@ -2,14 +2,18 @@
 
 namespace Drupal\stanford_earth_capx\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\config\Form\ConfigSingleImportForm;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
+use Drupal\Core\Serialization\Yaml;
+use Drupal\migrate_plus\Entity\Migration;
+use Drupal\Core\Batch\BatchBuilder;
+use Drupal\migrate\MigrateMessage;
+use Drupal\migrate_tools\MigrateBatchExecutable;
 
 /**
  * Contains Drupal\stanford_earth_capx\Form\EarthCapxUpdatePerson.
  */
-class EarthCapxUpdatePerson extends FormBase {
+class EarthCapxUpdatePerson extends ConfigSingleImportForm {
 
   /**
    * {@inheritdoc}
@@ -23,39 +27,50 @@ class EarthCapxUpdatePerson extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $form = parent::buildForm($form, $form_state);
+    // When this is the confirmation step fall through to the confirmation form.
+    if ($this->data) {
+      return $form;
+    }
+
     $form['stanford_earth_update_sunetid'] = [
       '#type' => 'textfield',
       '#title' => $this->t('SUNetID'),
       '#description' => $this->t('SUNetID of the profile being imported.'),
       '#required' => TRUE,
     ];
+    $form['config_type']['#default_value'] = 'migration';
+    $form['config_type']['#disabled'] = TRUE;
+    $form['advanced']['custom_entity_id']['#disabled'] = TRUE;
+    $form['import']['#default_value'] = 'unused';
+    $form['import']['#disabled'] = TRUE;
 
+    /*
     $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
-      ->loadByProperties(['vid' => 'people_search_terms']);
+    ->loadByProperties(['vid' => 'people_search_terms']);
     $term_list = [];
     foreach ($terms as $key => $value) {
-      $kids = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadChildren($key);
-      if (empty($kids)) {
-        $term_name = $value->getName();
-        if (substr($term_name,0, 4) !== 'All ') {
-          $term_list[$key] = $value->getName();
-        }
-      }
+    $kids = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadChildren($key);
+    if (empty($kids)) {
+    $term_name = $value->getName();
+    if (substr($term_name,0, 4) !== 'All ') {
+    $term_list[$key] = $value->getName();
+    }
+    }
     }
     asort($term_list);
 
     $form['stanford_earth_update_search_terms'] = [
-      '#type' => 'select',
-      '#title' => 'Directory Search Terms',
-      '#description' => 'Optional. Select terms for which this person is categorized in directory listings. Note: this gets reset every night based on workgroup membership.',
-      '#multiple' => TRUE,
-      '#options' => $term_list,
+    '#type' => 'select',
+    '#title' => 'Directory Search Terms',
+    '#description' => 'Optional. Select terms for which this person is ' .
+    'categorized in directory listings. Note: this gets reset every night ' .
+    'based on workgroup membership.',
+    '#multiple' => TRUE,
+    '#options' => $term_list,
     ];
 
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => 'Import',
-    ];
+     */
 
     return $form;
   }
@@ -63,13 +78,122 @@ class EarthCapxUpdatePerson extends FormBase {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $url = Url::fromUri('https://ksharp-earth.stanford.edu');
-    $form_state->setRedirectUrl($url);
-    // https://api.stanford.edu/profiles/v1 params see below
-    //$params = array('uids' => $sunet, 'ps'=>1, 'whitelist'=>'affiliations,displayName,shortTitle,bio,primaryContact,profilePhotos,longTitle,internetLinks,contacts,meta,titles');
-    //$data = ses_cap_lite_request('/profiles/v1',$params);
-    //print 'cap data for '.$sunet.'<br /><pre>'; print_r($data); print '</pre>';
+    $sunetid = $form_state->getValue('stanford_earth_update_sunetid');
+    $template = $this->configStorage->read('migrate_plus.migration.earth_capx_single_sunet');
+    if (!empty($template['uuid'])) {
+      unset($template['uuid']);
+    }
+    $batch_builder = new BatchBuilder();
+    $batch_builder->setTitle($this->t('Create Single Profile Migration'));
+    $batch_builder->setInitMessage($this->t('Creating profile import migrations for each requested workgroup.'));
+    $batch_builder->setProgressive(TRUE);
+    // Create migration configuration in batch mode.
+    $batch_builder->addOperation(
+      [
+        $this,
+        'earthCapxCreateSunetMigration',
+      ],
+      [
+        $form,
+        $form_state,
+        $sunetid,
+        $template,
+      ]);
+    $batch_builder->addOperation(
+      [
+        $this,
+        'earthCapxImportSunetProfile',
+      ],
+      [
+        $sunetid,
+      ]
+    );
+    $batch_builder->addOperation(
+      [
+        $this,
+        'earthCapxImportSunetCleanup',
+      ],
+      [
+        $sunetid,
+      ]
+    );
+    batch_set($batch_builder->toArray());
 
   }
+
+  /**
+   * Create migration config.
+   *
+   * @param array $form
+   *   Form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Form State array.
+   * @param string $sunetid
+   *   Sunetid of profile.
+   * @param array $fp_array
+   *   Migration definition array.
+   */
+  public function earthCapxCreateSunetMigration(array $form, FormStateInterface $form_state, string $sunetid, array $fp_array) {
+    $fp_array['id'] = 'earth_capx_single_sunet_' . $sunetid;
+    $fp_array['source']['urls'] = [
+      'https://cap.stanford.edu/cap-api/api/profiles/v1?uids=' . $sunetid .
+      '&ps=100&p=1&whitelist=affiliations,displayName,shortTitle,bio,primaryContact,' .
+      'profilePhotos,longTitle,internetLinks,contacts,meta,titles',
+    ];
+    $fp_array['label'] = 'Profile for ' . $sunetid;
+    $form_state->setValue('import', Yaml::encode($fp_array));
+    parent::validateForm($form, $form_state);
+    $config_importer = $form_state->get('config_importer');
+    $config_importer->import();
+  }
+
+  /**
+   * Import single sunet profile via batch.
+   *
+   * @param string $sunetid
+   *   Id of user to import.
+   */
+  public function earthCapxImportSunetProfile(string $sunetid) {
+    $migration = Migration::load('earth_capx_single_sunet_' . $sunetid);
+    /** @var \Drupal\migrate\Plugin\MigrationInterface $migration_plugin */
+    $migration_plugin = \Drupal::service('plugin.manager.migration')
+      ->createInstance($migration->id(), $migration->toArray());
+    $migration_plugin->getIdMap()->prepareUpdate();
+    $migrateMessage = new MigrateMessage();
+    $options = [
+      'limit' => 0,
+      'update' => 1,
+      'force' => 0,
+    ];
+    $executable = new MigrateBatchExecutable($migration_plugin, $migrateMessage, $options);
+    $executable->batchImport();
+  }
+
+  /**
+   * Cleanup migration of single sunet profile via batch.
+   *
+   * @param string $sunetid
+   *   Id of user to import.
+   */
+  public function earthCapxImportSunetCleanup(string $sunetid) {
+    // Delete the old migrations.
+    $this->configStorage->delete('migrate_plus.migration.earth_capx_single_sunet_' . $sunetid);
+    // Delete the old migration map and message tables.
+    $db = \Drupal::database();
+    $tables = array_merge($db->schema()
+      ->findTables('migrate_map_earth_capx_single_sunet_' . $sunetid),
+      $db->schema()->findTables('migrate_message_earth_capx_single_sunet_' . $sunetid));
+    foreach ($tables as $table) {
+      $db->schema()->dropTable($table);
+    }
+
+  }
+
 }
