c65a213e7eddffd26f3c90de3e22f9224b63fe4b
Jacob Wilson <jacobdeanwilson@gmail.com>
2021-07-09 21:37:07 +0000

    EARTH-1474: Fixups

--- modules/stanford_earth_contact_footer/src/Plugin/Block/EarthContactFooterBlock.php
+++ modules/stanford_earth_contact_footer/src/Plugin/Block/EarthContactFooterBlock.php
@@ -2,11 +2,7 @@
 
 namespace Drupal\stanford_earth_contact_footer\Plugin\Block;
 
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\term_condition\Plugin\Condition\Term;
 
 /**
  * Provides a block with a simple text.
@@ -27,28 +23,5 @@ class EarthContactFooterBlock extends BlockBase {
     ];
     
     return $build;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function blockAccess(AccountInterface $account) {
-    return AccessResult::allowedIfHasPermission($account, 'access content');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $config = $this->getConfiguration();
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->configuration['stanford_earth_contact_footer_settings'] = $form_state->getValue('stanford_earth_contact_footer_block_settings');
   }
 }
